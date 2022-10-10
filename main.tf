9fa123df52cdc6a851bcff54010ab18b97ed15bb
Mustafa Ali <mustafaali1128@gmail.com>
2022-10-10 20:41:44 +0000

    added website config

--- main.tf
+++ main.tf
@@ -27,3 +27,10 @@ resource "aws_s3_object" "website" {
   source = "/home/ec2-user/environment/s3bucketcv/index.html"
   key    = "website1"
   }
+
+resource "aws_s3_bucket_website_configuration" "example" {
+  bucket = aws_s3_bucket.cvwebsitemali.bucket
+
+  index_document {
+    suffix = "index.html"
+  }
