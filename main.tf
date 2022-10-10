ae208b287dcea55c86b4d106c12d6174a09d6256
Mustafa Ali <mustafaali1128@gmail.com>
2022-10-10 19:40:15 +0000

    uploaded the object

--- main.tf
+++ main.tf
@@ -12,18 +12,18 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "alicvwebsite" {
-  bucket = "ali-cv-website"
+resource "aws_s3_bucket" "cvwebsitemali" {
+  bucket = "cvwebsite-mali"
 
 }
 
 resource "aws_s3_bucket_acl" "alicv_bucket_acl" {
-  bucket = aws_s3_bucket.alicvwebsite.id
+  bucket = aws_s3_bucket.cvwebsitemali.id
   acl    = "public-read"
 }
 
 resource "aws_s3_object" "website" {
-  bucket = aws_s3_bucket.alicvwebsite.id
-  source = "/index.html"
+  bucket = aws_s3_bucket.cvwebsitemali.id
+  source = "/home/ec2-user/environment/s3bucketcv/index.html"
   key    = "website"
   }
